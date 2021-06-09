32d37874a3016322418eaa5838aa0de6a2936f4d
WallySa7 <dooody05314251@gmail.com>
2021-06-09 15:27:34 +0300

    Added POST, PUT, PATCH, DELETE

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import serializers, status
+from .serializers import HelloSerializer
 # Create your views here.
 
 
 class HelloApiView(APIView):
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -13,7 +16,21 @@ class HelloApiView(APIView):
             "Gives you the most control over your appliction logic",
             "Is mapped manually to URLs"
         ]
-
-        
-
         return Response({"message": "Hello!", "an_apiview": an_apiview})
+
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({"method": "PUT"})
+
+    def patch(self, request):
+        return Response({"method":"PATCH"})
+
+    def delete(self, request, pk=None):
+        return Response({"method":"delete"})
