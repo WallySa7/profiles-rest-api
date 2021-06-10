7c83a416ba152933c1587ce8e8a49c8dffb270d4
WallySa7 <dooody05314251@gmail.com>
2021-06-10 15:16:24 +0300

    Added Viewset to profiles api

--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
+from .models import UserProfile
+
 
 class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
 
+class Hello(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from .serializers import HelloSerializer
+from rest_framework import viewsets
+from .serializers import HelloSerializer, Hello
+from .models import UserProfile
 # Create your views here.
 
 
@@ -11,7 +15,7 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         an_apiview = [
-            "Uses HTTP methods as function (get, post, patch, put , delete)",
+            "Uses HTTP methods as function (get, post, patch, put, delete)",
             "Is similar to a tradition Django View",
             "Gives you the most control over your appliction logic",
             "Is mapped manually to URLs"
@@ -34,3 +38,35 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({"method":"delete"})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = Hello
+    def list(self, request):
+        queryset = UserProfile.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        user = UserProfile.objects.get(pk=pk)
+        serializer = self.serializer_class(user)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        user = UserProfile.objects.get(pk=pk)
+        serializer = self.serializer_class(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        user = UserProfile.objects.get(pk=pk)
+        user.delete()
+        return Response({"deleted":"it's got deleted"})
